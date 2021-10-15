4e280cd8e841a934806b848265feb16c32ab125e
Matt Rifaldi <mattrifaldi@gmail.com>
2021-10-15 17:10:44 +0700

    WIP

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::create([
+            'id' => 1,
             'name'	=> 'admin',
             'nik' => '123123123',
             'nama_lengkap' => 'Administrator',
