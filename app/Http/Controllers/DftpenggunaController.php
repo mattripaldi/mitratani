84db394d63b91b77be53a100780bbabe85af5a4a
Matt Rifaldi <mattrifaldi@gmail.com>
2021-10-15 10:17:08 +0700

    WIP

--- app/Http/Controllers/DftpenggunaController.php
+++ app/Http/Controllers/DftpenggunaController.php
@@ -6,7 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class dftpenggunaController extends Controller
+class DftpenggunaController extends Controller
 {
     /**
      * Display a listing of the resource.
