9db13dadaf328ed348f7b4e3e75a7cde6e2bb01c
Matt Rifaldi <mattrifaldi@gmail.com>
2021-11-25 06:19:05 +0700

    WIP

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,8 +1,48 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="container mb-2 mt-0">
-    <h1>WIP</h1>
+<div class="container mx-auto mb-2 mt-0">
+    <h1>Kebijakan Privasi</h1>
+    <p>Selamat datang di Mitra Tani store,
+        Dengan menggunakan layanan Aplikasi Web Mitra Tani Store dan Aplikasi Android Rojo Tani maka Anda setuju untuk terikat kepada syarat-syarat dan ketentuan berikut ini mengenai kebijakan privasi yang ditetapkan oleh Mitra Tani. Silahkan melakukan pengecekan berkala terhadap aplikasi kami untuk mendapatkan perubahan terbaru dan kemungkinan perubahan syarat dan kondisi sewaktu-waktu dari Kami.
+        </p>
+
+    <strong>
+        Pendaftaran Pelanggan
+
+    </strong>
+    <p>
+        Dengan melakukan pendaftaran sebagai Pelanggan, segala informasi terkait data Pelanggan dan data lahan adalah akurat dan benar apa adanya. Apabila informasi yang Anda berikan tersebut ternyata terbukti tidak benar, maka Kami tidak bisa bertanggung jawab atas akibat yang mungkin dapat terjadi.</p>
+    <p>
+Untuk keperluan pendaftaran sebagai Pelanggan di Mitra Tani Store, Kami memerlukan data-data antara lain sebagai berikut:
+    </p>
+
+<div>Nama Lengkap:</div>
+<div>No Telepon :</div>
+<div>Alamat:</div>
+<div>Jenis Kelamin :</div>
+<div>Alamat Lahan :</div>
+<div>Luas Lahan :</div>
+<div>Sejarah Lahan :</div>
+<div>Semua informasi yang Anda diberikan akan Kami simpan sebaik-baiknya dengan memperhatikan faktor keamanan data Pelanggan Kami.
+Informasi ini akan Kami pergunakan untuk:</div>
+
+    <ol>
+        <li>Memproses data pendaftaran yang anda ajukan</li>
+        <li>Melakukan komunikasi kepada Anda yang ingin mendapatkan informasi lebih lanjut mengenai Mitra Tani Store maupun Rojo Tani</li>
+        <li>Peningkatan layanan Mitra Tani Store dan Rojo Tani</li>
+    </ol>
+
+
+<strong>Kerahasiaan Data Pelanggan</strong>
+
+<p>Dengan ini Mitra Tani menyatakan tidak akan menyalahgunakan data Pelanggan atau menyebarluaskan data-data pribadi Pelanggan kepada orang lain dan/atau Pihak Ketiga yang tidak berkepentingan. Kecuali, dalam hal yang berkaitan dengan hukum dan diperintahkan oleh Pengadilan berdasarkan ketentuan Undang-Undang.
+    Sehubungan dengan hal tersebut, maka Anda menyetujui dan membebaskan Mitra Tani Store dari segala tuntutan, dan/atau gugatan yang berkaitan dengan hal tersebut.
+    </p>
+
+    <strong>Hukum</strong>
+<p>Syarat-syarat dan ketentuan dalam Kebijakan Privasi ini tunduk kepada hukum di wilayah Negara Kesatuan Republik Indonesia.</p>
+
 </div>
 
 
